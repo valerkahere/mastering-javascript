da2638e617f61b556778b4c7494a12843e19915f
Valerii Navalnyi <adultvalerka@gmail.com>
2025-12-29 08:54:55 +0000

    Make Subtraction Work

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let variable = 0;
 
-console.log(variable);
+// task 1
+let memLoc0 = 0;
+let memLoc1 = 1;
+
+let memLoc2 = memLoc1;
+memLoc2 = memLoc2 - 11;
+console.log(memLoc2);
+
+if (memLoc2 === 0) {
+
+}
