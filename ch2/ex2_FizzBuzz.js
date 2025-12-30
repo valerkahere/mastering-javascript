b5553164ddf5b2c73f6f066ced257cbc5fd5f58c
Valerii Navalnyi <adultvalerka@gmail.com>
2025-12-30 11:47:06 +0000

    ChEx2: Adapted for Leetcode Solution

--- ch2/ex2_FizzBuzz.js
+++ ch2/ex2_FizzBuzz.js
@@ -37,3 +37,25 @@ for (let index = 1; index < testCases.length; index++) {
     const testNumber = testCases[index];
     console.log(FizzBuzz(testNumber));
 }
+
+// Adapted for leetcode:
+console.log('Adapted FOR LEETCODE');
+const fizzBuzz = function(n) {
+    let answer = [];
+        for (let i = 1; i <= n; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                answer.push('FizzBuzz');
+            }
+            else if (i % 3 === 0) {
+                answer.push('Fizz');
+
+            } else if (i % 5 === 0) {
+                answer.push('Buzz');
+
+            } else {
+                answer.push(String(i));
+            }
+        }
+    return answer;
+};
+console.log(fizzBuzz(5));
