7e4b52f186f85dbbda91c651e256147964426d8d
Valerii Navalnyi <adultvalerka@gmail.com>
2025-12-30 10:47:36 +0000

    Test negating in alertTest

--- ch2/alertTest/index.js
+++ ch2/alertTest/index.js
@@ -1,3 +1,10 @@
+let yourName;
+yourName = prompt('Who are you?')
+console.log(!Boolean(yourName) == false);
+
+
+
+
 let theNumber = Number(prompt('Enter a number'));
 if (!Number.isNaN(theNumber)) {
     console.log(`Your number is the square root of ${theNumber * theNumber}`)
