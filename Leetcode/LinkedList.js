d299ef3eb0ed305d7a3c5c7c78ffaabc21e70adc
Valerii Navalnyi <adultvalerka@gmail.com>
2026-01-02 16:58:02 +0000

    Try LinkedLists

---
+++ Leetcode/LinkedList.js
@@ -0,0 +1,40 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        let newnode = new Node(value);
+        if(!this.head) {
+            this.head = newnode;
+            return
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newnode;
+    }
+    printList() {
+        let current = this.head;
+        let result = '';
+        while(current) {
+            result += current.value+',';
+            current = current.next;
+        }
+        console.log(result + 'null');
+    }
+    delete(value) {
+        
+    }
+}
+
+let list = new LinkedList();
+list.append(10);
+list.printList();
